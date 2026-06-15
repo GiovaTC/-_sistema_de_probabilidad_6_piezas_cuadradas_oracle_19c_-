282c3d7c26b9b18e2e82712f4dbd8787a5442350
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-15 09:54:01 -0500

    commit - ConexionOracle / .

--- src/conexion/ConexionOracle.java
+++ src/conexion/ConexionOracle.java
@@ -1,4 +1,30 @@
 package conexion;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
 public class ConexionOracle {
-}
+
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private static final String USER = "system";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() {
+
+        try {
+            Class.forName(
+                    "oracle.jdbc.driver.OracleDriver");
+
+            return DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return null;
+        }
+    }
+}   
